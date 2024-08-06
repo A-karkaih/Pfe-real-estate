b32281e5c126f4dbd1e4f08ecaf0368f8b060db0
A-karkaih <akarkaih@gmail.com>
2024-08-06 19:49:09 +0100

    middleware and function to handleErrors

--- backend/index.js
+++ backend/index.js
@@ -16,3 +16,15 @@ app.listen(PORT, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth" , AuthRouter);
+
+//midlware for handling errors
+
+app.use((err,req,res,next)=> {
+const statusCode = err.statusCode || 500 ;
+const message = err.message || "Internal Server Error";
+return res.status(statusCode).json({
+  success : false ,
+  statusCode,
+  message ,
+})
+})
