1c33ab40434a3b41f9917663a3a5d8f2bcd23856
A-karkaih <akarkaih@gmail.com>
2024-08-06 20:05:39 +0100

    complete sign up page ui

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,6 @@ app.use("/api/user", userRouter);
 app.use("/api/auth" , AuthRouter);
 
 //midlware for handling errors
-
 app.use((err,req,res,next)=> {
 const statusCode = err.statusCode || 500 ;
 const message = err.message || "Internal Server Error";
