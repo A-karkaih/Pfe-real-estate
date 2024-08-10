48c6e8d9d0fe4ffd293956c2a4bef7d4ab267b06
A-karkaih <akarkaih@gmail.com>
2024-08-10 17:47:24 +0100

    redux persist

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -34,7 +34,7 @@ const SignIn = () => {
         dispatch(signInFailure(data.message));
         return;
       }
-      dispatch(signInSuccess());
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
       dispatch(signInFailure(error.message));
