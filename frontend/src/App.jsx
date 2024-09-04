689a153fe2ed79c18e19ec5be5bd24c2c1cd6e6d
A-karkaih <akarkaih@gmail.com>
2024-09-04 19:37:53 +0100

    update/delete /showing listings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from "./pages/Profile";
 import NavBar from "./components/NavBar";
 import PrivatRoute from "./components/PrivatRoute";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
         <Route element={<PrivatRoute />}> 
         <Route path="/profile" element={<Profile />} />
         <Route path='/create-listing' element={<CreateListing />} />
+        <Route path='/update-listing/:listingId' element={<UpdateListing />} />
 
         </Route>
        
