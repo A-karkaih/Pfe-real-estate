9fe5c7cdae4016e3dc82d4030fec1f8a0239c1fb
A-karkaih <akarkaih@gmail.com>
2024-09-11 17:40:38 +0100

    deployment

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,12 @@ import { connectDB } from "./config/DB.js";
 import userRouter from "./routes/user.route.js";
 import AuthRouter from "./routes/auth.route.js"
 import ListingRouter from "./routes/listing.route.js"
+import path from 'path';
+
 const app = express();
+
+const __dirname = path.resolve();
+
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
@@ -20,6 +25,11 @@ app.listen(PORT, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth" , AuthRouter);
 app.use("/api/listing" , ListingRouter);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+})
 
 
 //midlware for handling errors
